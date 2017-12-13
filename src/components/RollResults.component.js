e6d4413cd057a8cb27aa51f8df07fecdd3cae283
TREMORLOR\calcidern-laptop <kadadel@gmail.com>
2017-12-13 17:28:02 +0100

    updted dice result view

--- src/components/RollResults.component.js
+++ src/components/RollResults.component.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import {List, ListItem} from 'material-ui/List';
-import {Chip} from "material-ui";
+import {Chip, RaisedButton} from "material-ui";
 
 export default ({results}) => {
   const resultStr = `Results: ${results.diceResults.map(e => e.join(',')).join('||')}`;
@@ -8,13 +8,17 @@ export default ({results}) => {
   const totalStr = `Total: ${results && results.sumAll}`;
   const resultItems = results.diceResults.map((em, i) => (
     <span key={i} style={{display: 'inline-flex', flexWrap: 'wrap',margin:'0 0.25em'}}>
-      {em.map((e, j) => <Chip key={10 * i + j} style={{borderRadius:'0.5em', margin:'0.1em'}}>{e}</Chip>)}
+      {em.map((e, j) => <RaisedButton key={10 * i + j}
+                                      style={{minWidth:'36px', margin:'0 0.2em'}}
+                                      overlayStyle={{padding:'0 0.5em'}}>
+        {e}
+        </RaisedButton>)}
     </span>
   ));
   return (
     <div>
       <List>
-        <ListItem>
+        <ListItem innerDivStyle={{paddingBottom:4,paddingTop:4}}>
           Results: {resultItems}
         </ListItem>
         {results.sumEach.length > 1 && <ListItem primaryText={eachStr}/>}
