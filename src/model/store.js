674cf06a3a0915703ecd0cbf450759b4641b7b7d
calcidern <kadadel@gmail.com>
2018-06-23 21:38:13 +0200

    router  fix

--- src/model/store.js
+++ src/model/store.js
@@ -5,7 +5,9 @@ import createHistory from 'history/createBrowserHistory'
 import rootReducer from './rootReducer'
 import {composeWithDevTools} from 'redux-devtools-extension'
 
-export const history = createHistory();
+export const history = createHistory({
+  basename: process.env.PUBLIC_URL,
+});
 
 const initialState = {};
 const enhancers = [];
